56a9c8a31f07493df71742c0183afd2f67e2122c
wqz111 <50622008+wqz111@users.noreply.github.com>
2019-12-22 15:01:08 +0800

    Add files via upload

--- delete_interpolation.py
+++ delete_interpolation.py
@@ -12,7 +12,6 @@ Inputs:
 vocab_num: 词汇数量
 word_num: 在给定历史条件下单词出现的次数
 hist_num: 历史条件出现的次数
-
 Returns:
 new_p: 加值平滑后的词汇条件概率
 '''
@@ -28,7 +27,6 @@ def add_smoothing(vocab_num, word_num, hist_num):
 Inputs: 
 gram_num: gram的数量
 p_list: 不同gram的值列表 shape = (gram_num, probability_num)
-
 Returns:
 p_2gram_matrix: 插值平滑一轮后的gram概率矩阵
 '''
@@ -58,7 +56,7 @@ def delete_interpolation(gram_num, p_list, word2id, test_filename):
     # print(corpus_p)
     new_cross_entropy = cross_entropy(vocab_num, corpus_p)
 
-    while (old_cross_entropy != new_cross_entropy):
+    while (abs(old_cross_entropy - new_cross_entropy) > 1):
         old_cross_entropy = new_cross_entropy
         p_2gram_matrix = round(gram_list, p_1gram_matrix, p_2gram_matrix, word2id)
         corpus_p = prob_bigram_T(f, word2id, p_1gram_matrix, p_2gram_matrix)
@@ -100,7 +98,6 @@ gram_list[n][0]是gram内容
 gram_list[n][1]是出现次数
 p_1gram_matrix: unigram概率矩阵
 p_2gram_matrix: bigram概率矩阵
-
 Returns:
 p_2gram_matrix: 更新的bigram概率矩阵
 '''
@@ -135,14 +132,14 @@ def round(gram_list, p_1gram_matrix, p_2gram_matrix, word2id):
             p_2gram = get_p_with_bigram(word1, word2, p_2gram_matrix, word2id)
             p_new = x * p_1gram + (1 - x) * p_2gram
             #p_case[history] = p_case[history] * pow(p_new, gram_count)
-            p_case[history] = p_case[history] * gram_count * sympy.log(p_new)
+            p_case[history] = p_case[history] + gram_count * sympy.log(p_new)
             # p_3gram = get_p()
 
     for history in p_case:
         # print('ready to calculate')
         #x_temp = 0
         x_result = 0
-        learning_rate = 0.1
+        learning_rate = 0.05
         #p_case_t = p_case[history].subs(x, 0)
         x_list = []
         p_list = []
@@ -193,7 +190,6 @@ Inputs:
 x: 插值参数
 history: gram历史
 p_matrix: gram概率矩阵
-
 Returns:
 updated_gram_matrix: 更新过的gram概率矩阵
 '''
@@ -214,7 +210,6 @@ def update_gram_matrix_by_history(x, history, p_2gram_matrix, p_1gram_matrix):
 Inputs:
 gram: gram的内容
 gram_matrix: gram概率矩阵
-
 Returns:
 p: gram的概率  # []
 '''
@@ -232,7 +227,6 @@ def get_p_with_unigram(word, unigram, word2id):
 Inputs:
 gram: gram的内容
 gram_matrix: gram概率矩阵
-
 Returns:
 p: gram的概率  # []
 '''
@@ -286,19 +280,19 @@ def generate_gram_list(f):
     return gram_list
 
 if __name__ == "__main__":
-    f = 'train.txt'
-    test_filename = 'test.txt'
+    f = 'train'
+    test_filename = 'test'
     flist = f_original_shape(f)
-    num=get_vocab_num(f)
+    num=get_vocab_num(test_filename)
     counter = generate_counter_list(flist)
     word2id = get_word2id(counter)
     unigram = get_p_1gram_matrix(f)
     bigram = get_p_2gram_matrix(f)
-    np.savetxt("old_bigram.txt", bigram,fmt='%f',delimiter=',')
+    #np.savetxt("old_bigram.txt", bigram,fmt='%f',delimiter=',')
     p_list = [unigram, bigram]
     bigram = delete_interpolation(2, p_list, word2id, test_filename)
     np.savetxt("new_bigram.txt", bigram,fmt='%f',delimiter=',')
-
+    #bigram = np.loadtxt("new_bigram.txt", delimiter=',')
     res_T=prob_bigram_T(test_filename,word2id,unigram,bigram)
     cross_entropy_res=cross_entropy(num,res_T)
     pp=perplexity(cross_entropy_res)
